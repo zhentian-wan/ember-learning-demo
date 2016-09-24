510ad2467c476fecb178d98a1acfaa52d561453f
Zhentian Wan <answer881215@gmail.com>
2016-09-24 17:56:27 +0300

    init push

---
+++ tests/unit/controllers/hello-test.js
@@ -0,0 +1,12 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('controller:hello', 'Unit | Controller | hello', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  let controller = this.subject();
+  assert.ok(controller);
+});
